5a53bbe265309887ec702d20f2c3a93ad927e6a3
Robert Starmer <starmer@cisco.com>
2012-07-20 02:19:00 -0700

    added tempest image facts

---
+++ lib/facter/tempest_image_1.rb
@@ -0,0 +1,14 @@
+# glance_images.rb
+# glance managed images facts
+
+Facter.add(:tempest_image_1) do
+  setcode do
+    images = []
+    output = Facter::Util::Resolution.exec("glance index -I admin -K Cisco123 -T openstack -N http://192.168.200.40:5000/v2.0/ | grep tempest-1")
+    output.each_line do |line|
+      split_line = line.split(" ")[0]
+      images.push(split_line) 
+    end
+    images[0]
+  end
+end
